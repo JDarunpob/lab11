d58f6d8d1caf5e0fc229578af39690192e9f0fa3
JDarunpob <darunpob.jean@gmail.com>
2024-12-20 16:38:37 +0700

    complete

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,39 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+ #include<iostream>
+ #include<string>
+ #include<cstdlib>
+ #include<ctime>
+ using namespace std;
+
+ 
+ 
+ int main(){
+    srand(time(0));
+    int i=0;
+    string x;
+    cout << "Press Enter 3 times to reveal your future.\n";
+    cin.get();
+    cin.get();
+    cin.get();
+    i = rand()%9 + 1;
+    if(i==1){
+        x = "A";
+    }else if(i==2){
+        x = "B+";
+    }else if(i==3){
+        x = "B";
+    }else if(i==4){
+        x = "C+";
+    }else if(i==5){
+        x = "C";
+    }else if(i==6){
+        x = "D+";
+    }else if(i==7){
+        x = "D";
+    }else if(i==8){
+        x = "F";
+    }else if(i==9){
+        x = "W";
+    }
+    cout << "You will get "<<x<<" in this 261102.";
+
+ }
